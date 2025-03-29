dc6e3680a811cc58b40c60c0b5cb9abc9c76a62a
n4nm4r <nancy.martin.nb@gmail.com>
2025-03-29 14:44:09 -0300

    Add project files.

---
+++ ticketsAPI/Controllers/TicketsController.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text.Json;
+
+namespace ticketsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ILogger<TicketsController> _logger;
+
+        private readonly IConfiguration _configuration;
+
+
+        public TicketsController (ILogger<TicketsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get() {
+            return Ok("Returned Ok (Get)");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Ticket ticket) {
+            //Validation
+
+            if (ModelState.IsValid == false) { return BadRequest(ModelState); }
+
+            //send to azure
+
+            string queueName = "tickethub";
+            // Get connection string from secrets.json
+            string? connectionString = _configuration["AzureStorageConnectionString"];
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return BadRequest("An error was encountered");
+            }
+
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            // serialize an object to json
+            string message = JsonSerializer.Serialize(ticket);
+           
+
+            // send string message to queue
+            await queueClient.SendMessageAsync(message);
+
+            return Ok("Returned Ok (Post)\n" + ticket.Quantity + " tickets for " + ticket.Name);
+        }
+
+
+
+    }
+}
---
+++ ticketsAPI/Ticket.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ticketsAPI
+{
+    public class Ticket
+    {
+
+        [Required]
+        public int ConcertID { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(60)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 12, ErrorMessage = "Quantity must be between 1 and 12.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [CreditCard]
+        public string CreditCard { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"\d{2}/\d{2}", ErrorMessage = "Expiration must be in the format MM/YY.")]
+        public string Expiration { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"\d{3,4}", ErrorMessage = "Security code must be 3 or 4 digits.")]
+        public string SecurityCode { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string Province { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d", ErrorMessage = "Postal code must be in the format A1B 2C3.")]
+        public string PostalCode { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string Country { get; set; } = string.Empty;
+    }
+}
